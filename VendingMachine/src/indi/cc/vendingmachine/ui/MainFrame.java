9eff15259e96872ba7cdc082e3734f2c71366ff2
Makoto87 <makosinhori@gmail.com>
2021-06-02 12:07:11 +0900

    add dialog view

--- VendingMachine/src/indi/cc/vendingmachine/ui/MainFrame.java
+++ VendingMachine/src/indi/cc/vendingmachine/ui/MainFrame.java
@@ -1,6 +1,12 @@
 package indi.cc.vendingmachine.ui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -10,11 +16,11 @@ import java.util.Vector;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
@@ -36,7 +42,7 @@ public class MainFrame extends javax.swing.JFrame {
     private JLabel jLabel4;
     private JLabel jLabel5;
     private JPanel jPanel1;
-    private JScrollPane jScrollPane1;
+//    private JScrollPane jScrollPane1;
     private JSeparator jSeparator1;
     private JSeparator jSeparator2;
     private JTextField jTextField1;
@@ -70,20 +76,18 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel3 = new JLabel();
         jLabel4 = new JLabel();
         jPanel1 = new JPanel();
-        jScrollPane1 = new JScrollPane(jPanel1);	// スクロール可能なビュー
         jSeparator1 = new JSeparator();
         jSeparator2 = new JSeparator();
         jLabel5 = new JLabel();
-        jPanel1.setLayout(new GridLayout(0,2));	// グリッドレイアウトを設定する。2列の表示になる。行は任意の数
+        jPanel1.setLayout(new GridLayout(0,3));	// グリッドレイアウトを設定する。2列の表示になる。行は任意の数
       
         // WindowConstants -> ウィンドウのクローズ操作を制御する定数
         // EXIT_ON_CLOSE -> 終了するアプリケーションのデフォルトのウィンドウにクローズ・オペレーション
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);	// デフォルトのクローズ操作の指定
         setTitle("自動販売機");	// タイトル(画面最上部)
-        setBackground(new java.awt.Color(245, 140, 100));	// 背景色をRGBで設定、変えても変化なし
+        setBackground(new java.awt.Color(245, 140, 100));	// タイトルバーをRGBで設定
+        jPanel1.setBackground(Color.GRAY);
 
-        // 直線ボーダーを作成し、スクロールパネルに追加
-        jScrollPane1.setViewportBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
         // 数字のみの入力制限（テンプレートコード）
         // キーボードが押されたときのイベントを追加
@@ -101,7 +105,7 @@ public class MainFrame extends javax.swing.JFrame {
         });
         
         jButton1.setFont(new java.awt.Font("幼圆", 1, 12)); // ボタンのフォントを指定
-        jButton1.setText("管理人入り口");	// ボタンのテキストを指定
+        jButton1.setText("管理人入口");	// ボタンのテキストを指定
         
         // 登録の問題
         // ボタンにアクションイベントを受け取れるようにする
@@ -115,7 +119,10 @@ public class MainFrame extends javax.swing.JFrame {
 				
 			}
 		});
-        jLabel1.setText("お金投入口：");	// 文字の指定
+//        jLabel1.setText("お金投入口：");	// 文字の指定
+        jLabel1.setText("買いたい商品を選んでください");	// 文字の指定
+        jLabel1.setOpaque(true);
+        jLabel1.setBackground(Color.YELLOW);
 
         jButton2.setFont(new java.awt.Font("幼圆", 1, 15)); // フォントを指定
         jButton2.setText("お金を確認");		// 文字の指定
@@ -133,59 +140,31 @@ public class MainFrame extends javax.swing.JFrame {
 		});
        
         // 文字の記入
-        jLabel2.setText("还应投入：");
+        jLabel2.setText("金額：");
 
         jLabel3.setText("0元");
 
-        jLabel4.setText("选中的饮料：");
+        jLabel4.setText("ドリンク：");
 
         // フレームはペインと呼ばれる層が重なって成り立っている
         // getContentPane(ボタンなどのコンポーネントを含める層)を取得
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);	// layoutを設定
         
+        
         // 水平軸に沿ってコンポーネントの配置とサイズを指定するように設定
         // 横方向の要素の整列
         GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
         hGroup.addGroup(
 	    		layout.createParallelGroup()
-	    		.addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 540, javax.swing.GroupLayout.PREFERRED_SIZE)
-	//        		.addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 419, javax.swing.GroupLayout.PREFERRED_SIZE)
-//	                .addGap(18, 18, 18)
-	            .addComponent(jButton1)
-	            .addComponent(jLabel2)
-	            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-	            .addComponent(jLabel1)
-	            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
-	            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addComponent(jLabel4)
-                .addComponent(jSeparator1)
-                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+	    		.addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
+	            .addGroup(layout.createSequentialGroup()
+	            		.addGap(300, 310, 320)
+	            		.addComponent(jLabel1)
+	            		.addGap(160, 170, 180)
+	            		.addComponent(jButton1)
+	            		.addGap(5, 10, 20))
     	);
-        
-   
-    
-//    	hGroup.addGroup(
-//    			layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-//                // ラベル
-//                .addComponent(jLabel2)
-//                .addGap(10, 10, 10)
-//                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-//                
-//                .addComponent(jLabel1)
-//                .addGap(18, 18, 18)
-//                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
-//                
-//                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-//                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-//                .addComponent(jLabel4)
-//                .addComponent(jSeparator1)
-//                .addGap(5, 5, 5)
-//                
-//                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-//                
-//        );
         
         layout.setHorizontalGroup(hGroup);
         
@@ -194,55 +173,14 @@ public class MainFrame extends javax.swing.JFrame {
         GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
         vGroup.addGroup(
         		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        		.addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 419, javax.swing.GroupLayout.PREFERRED_SIZE)
+        		.addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         
         vGroup.addGroup(
         		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+        		.addComponent(jLabel1)
         		.addComponent(jButton1)
                 .addGap(10, 10, 10)
-        );
-        
-        vGroup.addGroup(
-        		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        		.addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(44, 44, 44)
-        );
-        
-        vGroup.addGroup(
-        		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        		.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jLabel2)
-                        .addComponent(jLabel3))
-                    .addGap(33, 33, 33)
-        );
-        
-        vGroup.addGroup(
-        		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        		.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(3, 3, 3)
-                            .addComponent(jLabel1))
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGap(18, 18, 18)
-        );
-        
-        vGroup.addGroup(
-        		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        		.addComponent(jButton2)
-                .addGap(10, 10, 10)
-        );
-        
-        vGroup.addGroup(
-        		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        		.addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(4, 4, 4)
-        );
-        
-        vGroup.addGroup(
-        		layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        		 .addComponent(jLabel4)
-        		 .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         
         layout.setVerticalGroup(vGroup);
@@ -253,48 +191,104 @@ public class MainFrame extends javax.swing.JFrame {
     
     private void upateJpanel(){
     	AdminManageHelper manageHelper = new AdminManageHelper();
-    	Vector<Drink> drinks = manageHelper.getAllDrink();	//获得所有的饮料
+    	Vector<Drink> drinks = manageHelper.getAllDrink();	// 飲み物をすべて手に入れる
     	 for(int i=0;i<drinks.size();i++){
          	Drink drink = drinks.get(i);   	
          	String s;
-         	JButton jb;//按钮
-         	//对数量为0的饮料进行判断并且处理
+         	JButton jb;	// ボタン
+         	// 数量0の飲料を判定・加工
          	if(drinks.get(i).getQuantity()==0){
-         		s = drink.getDrinkName()+"|已售空";
+         		s = drink.getDrinkName()+"|完売";
          		jb = new JButton(s,new ImageIcon(drink.getDrinkImg()));
-         		jb.setEnabled(false);//设置不可用
+         		jb.setEnabled(false);	// 利用できない状態
          	}else{
-         		s = drink.getDrinkName()+"|价格:"+drink.getPrice()+"元|数量:"+drink.getQuantity();
+         		s = drink.getDrinkName()+"|価格:"+drink.getPrice()+"元|数量:"+drink.getQuantity();
          		jb = new JButton(s,new ImageIcon(drink.getDrinkImg()));
          	}
-         	//对饮料机的零钱小于10的时候(无法找零的时候)进行判断和处理
+         	// 飲料機の変化が10回未満の場合（変化が見られない場合）を判定・処理
          	if(VendingMachine.getInstance().getCoin()<10){
-         		jb.setEnabled(false);	//设置不可用。
+         		jb.setEnabled(false);	// 利用できない状態
          	}
+         	jb.setOpaque(true);
+            jb.setBackground(Color.GRAY);
          	
          	jb.addActionListener(new ActionListener() {
  				
  				@Override
  				public void actionPerformed(ActionEvent e) {
- 					// TODO Auto-generated method stub
+ 					
  					if(pay!=0){
- 						JOptionPane.showMessageDialog(thisJframe, "请完成支付!!");
+ 						JOptionPane.showMessageDialog(thisJframe, "支払いを完了してください!!");
  					}else{
 	 					chooseDrink = drink;
 	 					jLabel5.setIcon((new ImageIcon(drink.getDrinkImg())));
 	 					jLabel3.setText(drink.getPrice()+"元");
+	 					jLabel4.setText(drink.getDrinkName());
 	 					price = drink.getPrice();
-	 					need = price;	//设置还需的金额
+	 					need = price;	// 残額を設定する
 	 					jLabel5.updateUI();
  					}
+
+ 			        // モーダルで表示
+ 					JDialog dialog = new JDialog(thisJframe, true);
+ 					dialog.setLayout(new GridBagLayout());
+ 					
+ 					GridBagConstraints gbc = new GridBagConstraints();
+ 					gbc.gridx = 0;
+ 					gbc.gridy = 0;
+ 					
+ 					// 画像と商品の説明のレイアウト
+ 					JPanel panel1 = new JPanel();
+ 					panel1.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+ 					panel1.add(jLabel5);
+ 					
+ 					JPanel panel1_1 = new JPanel();
+ 					panel1_1.setLayout(new GridLayout(0, 1, 10, 10));
+ 					panel1_1.add(jLabel4);
+ 					panel1_1.add(jLabel3);
+ 					panel1_1.add(new Label("を選択中"));
+ 					panel1_1.add(jTextField1);
+ 					
+ 					panel1.add(panel1_1);
+ 					
+ 					dialog.add(panel1, gbc);
+ 					
+ 					// フッターのレイアウト
+ 					JPanel panel2 = new JPanel();
+ 					panel2.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+ 					
+ 					JButton jButton3 = new JButton("戻る");
+// 					jButton3.setText("戻る");
+ 					jButton3.addActionListener(new ActionListener() {
+ 						
+ 						@Override
+ 						public void actionPerformed(ActionEvent e) {
+ 							dialog.dispose();
+ 						}
+ 					});
+ 					
+ 					panel2.add(jButton3);
+ 					panel2.add(jButton2);
+ 					
+ 					gbc.gridx = 0;
+ 					gbc.gridy = 1;
+ 					dialog.add(panel2, gbc);
+ 					
+ 					jLabel2.setPreferredSize(new Dimension(100, 70)); 					
+ 					
+ 					dialog.setResizable(false);
+ 					dialog.setLocationRelativeTo(null);
+ 					dialog.pack();
+ 					dialog.show(); 					
  				}
  			});
+         	
          	jPanel1.add(jb);
          }
     }
     
     
-    //计算处理投币的方法
+    // コインの取り扱い方法を計算する
     private void computing(){
     	int repayment = 0;	//找回金额
 		int oncePay = 0;	//一次支付金额
@@ -306,15 +300,15 @@ public class MainFrame extends javax.swing.JFrame {
 		if(price==0){
 			
 			if(oncePay!=0){
-				JOptionPane.showMessageDialog(thisJframe, "请选择一款饮料再投币!!");
-				JOptionPane.showMessageDialog(thisJframe, "请您取回你的钱!!");
+				JOptionPane.showMessageDialog(thisJframe, "ドリンクを選んでコインを追加してください!!");
+				JOptionPane.showMessageDialog(thisJframe, "お金を返してください!!");
 				jTextField1.setText("");
 			}else{
-				JOptionPane.showMessageDialog(thisJframe, "请选择一款饮料!!");
+				JOptionPane.showMessageDialog(thisJframe, "ドリンクを選んでください!!");
 				jTextField1.setText("");
 			}
 		}else if(price!=0 && oncePay==0){
-			JOptionPane.showMessageDialog(thisJframe, "请投币!!");
+			JOptionPane.showMessageDialog(thisJframe, "コインをください!!");
 			jTextField1.setText("");
 		}else{
 			
@@ -322,7 +316,7 @@ public class MainFrame extends javax.swing.JFrame {
 			case 1:
 					need = need - 1;
 					if(need!=0){
-						JOptionPane.showMessageDialog(thisJframe, "还需投入："+need+"元");
+						JOptionPane.showMessageDialog(thisJframe, "投資が必要："+need+"元");
 					}
 					pay += oncePay;	//得到一共支付的金额
 					break;
@@ -333,7 +327,7 @@ public class MainFrame extends javax.swing.JFrame {
 					}else{//支付的5元钱小于于还需的金额,还需继续付钱
 						need = need - 5;	
 						if(need!=0){
-							JOptionPane.showMessageDialog(thisJframe, "还需投入："+need+"元");
+							JOptionPane.showMessageDialog(thisJframe, "投資が必要："+need+"元");
 						}
 					}
 					pay += oncePay;	//得到一共支付的金额
@@ -342,24 +336,24 @@ public class MainFrame extends javax.swing.JFrame {
 				    need -= 10;
 					if(need!=0){
 
-						JOptionPane.showMessageDialog(thisJframe, "还需投入："+need+"元");
+						JOptionPane.showMessageDialog(thisJframe, "投資が必要："+need+"元");
 					}
 					pay += oncePay;	//得到一共支付的金额
 					break;
 					
 			default:
-					JOptionPane.showMessageDialog(thisJframe, "请投入1元硬币或5元纸币,10元纸币!!");
-					JOptionPane.showMessageDialog(thisJframe, "请您取回你的钱!!");
+					JOptionPane.showMessageDialog(thisJframe, "1元硬貨または5元札、10元札を入れてください!!");
+					JOptionPane.showMessageDialog(thisJframe, "お金を返してください!!");
 					jTextField1.setText("");
 					break;
 			}
 			
 			
 			if(repayment!=0){
-				JOptionPane.showMessageDialog(thisJframe, "一共找回您："+repayment+"元");
+				JOptionPane.showMessageDialog(thisJframe, "完全に取り戻す："+repayment+"元");
 			}
 			if(price!=0 && need==0){//付款成功
-				JOptionPane.showMessageDialog(thisJframe, "您已经成功购买该饮料,请等待出柜!!");
+				JOptionPane.showMessageDialog(thisJframe, "ドリンクの購入に成功しました。出てくるまでお待ちください!!");
 				//对应的饮料数量需要减少!!
 				//顾客操作的帮助类实现饮料数量的减少
 				CustomerManageHelper helper = new CustomerManageHelper();
